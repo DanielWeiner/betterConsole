c0930e066354eabe2734612dcc6e1ee123639296
Daniel Weiner <daniel.weiner@dreamtown.com>
2015-01-27 15:06:55 -0600

    set up betterConsole.js

---
+++ betterConsole.js
@@ -0,0 +1,24 @@
+var betterConsole = (function(){
+	var intros = 
+		[
+			"Check this shit out:",
+			"YO!",
+			"Fuck yeah,",
+			"OH DEAR GOT IT'S",
+			"It's a bird, it's a plane, it's"
+		];
+
+	consoleLog = console.log.bind(console);
+	return {
+		start: function() {
+			console.log = function(){
+				var args = [].slice.call(arguments);
+				consoleLog.apply(console, [intros[~~(Math.random() * intros.length)]].concat(args))
+			}
+		},
+		stop: function() {
+			console.log = consoleLog;
+		}
+	}
+
+}());
